13533be050ca113d3835eebe222996f1d8affe06
g7vind <codingacc408@gmail.com>
2023-08-25 19:08:26 +0530

    updated

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-var var1 = document.querySelector("#pass1");
-var var2 = document.querySelector("#pass2");
-isAlive = false;
-
+var var1 = document.querySelector("#pass1"),
+var2 = document.querySelector("#pass2");
 function passg() {
-  var s =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>,.?/+_-*&^%$#@!=";
-  var sb = "";
-  for (var i = 0; i < 12; i++) {
-    sb += s.charAt(Math.floor(Math.random() * s.length));
-  }
-  return sb;
+  for (var e = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>,.?/+_-*&^%$#@!=", a = "", r = 0; r < 12; r++) a += e.charAt(Math.floor(Math.random() * e.length));
+  return a
 }
-
-function copy(x) {
-  if (isAlive == false) {
-    alert("Please generate a password first");
-  } else {
-    if (x == 1) var1.select();
-    else var2.select();
-    document.execCommand("copy");
-    alert("Password copied to clipboard");
-  }
+function copy(e) {
+  0 == isAlive ? alert("Please generate a password first") : ((1 == e ? var1 : var2).select(), document.execCommand("copy"), alert("Password copied to clipboard"))
 }
 function generate() {
-  isAlive = true;
-  var1.value = passg();
-  var2.value = passg();
+  isAlive = !0, var1.value = passg(), var2.value = passg()
 }
+isAlive = !1;
